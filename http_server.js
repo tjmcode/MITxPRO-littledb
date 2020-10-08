5968e443fb9a26ab9979f1857b54c9f783dd727a
abelsan <abel@mit.edu>
2020-10-08 16:07:58 -0400

    live class code

--- http_server.js
+++ http_server.js
@@ -1,7 +1,5 @@
-// add http server
-// -----------------------
-// YOUR CODE
-
+var express = require('express');
+var app     = express();
 var low     = require('lowdb');
 var fs      = require('lowdb/adapters/FileSync');
 var adapter = new fs('db.json');
@@ -9,16 +7,14 @@ var db      = low(adapter);
 
 // configure express to serve static files from public directory
 // ------------------------------------------------------------------
-// YOUR CODE
+app.use(express.static('public'))
 
 // init the data store
 db.defaults({ posts: []}).write();
 
 // list posts
 app.get('/data', function(req, res){     
-
-    // YOUR CODE
-
+    res.send(db.get('posts').value());
 });
 
 // ----------------------------------------------------
@@ -26,9 +22,15 @@ app.get('/data', function(req, res){
 //      curl http://localhost:3000/posts/ping/1/false
 // ----------------------------------------------------
 app.get('/posts/:title/:id/:published', function(req, res){
-
-    // YOUR CODE
-
+    var post = {
+        "id"    : req.params.id,
+        "title" : req.params.title,
+        "published" : req.params.published
+    }
+    console.log(post);
+    db.get('posts').push(post).write();
+    console.log(db.get('posts').value());
+    res.send(db.get('posts').value());
 });
 
 // ----------------------------------------------------
@@ -63,4 +65,6 @@ app.get('/delete/:id/', function(req, res){
 
 // start server
 // -----------------------
-// YOUR CODE
+app.listen(3000, function(){
+    console.log('Running at port 3000');
+})
