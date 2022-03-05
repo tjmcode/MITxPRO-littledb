49ddac91edc811bbc60c685708bca915df83b06c
Timothy J McGuire <tmcguire@mcode.com>
2022-03-05 07:47:24 -0600

    My updates from class excercises

--- http_server.js
+++ http_server.js
@@ -1,70 +1,91 @@
-var express = require('express');
-var app     = express();
-var low     = require('lowdb');
-var fs      = require('lowdb/adapters/FileSync');
-var adapter = new fs('db.json');
-var db      = low(adapter);
+/**
+ * http_server.js -- JavaScript to build an Hypertext Transport Protocol (HTTP) Server.
+ * --------------
+ *
+ */
+
+var express = require("express"); // the code for our 'Server'
+var app = express(); // starts 'ExpressJS' Application
+var low = require("lowdb");
+var fs = require("lowdb/adapters/FileSync");
+var adapter = new fs("db.json");
+var db = low(adapter);
 
 // configure express to serve static files from public directory
-// ------------------------------------------------------------------
-app.use(express.static('public'))
+// -------------------------------------------------------------
+app.use(express.static("public"));
 
 // init the data store
-db.defaults({ posts: []}).write();
+db.defaults({ posts: [] }).write();
 
-// list posts
-app.get('/data', function(req, res){     
-    res.send(db.get('posts').value());
+/**
+ * R O U T E S
+ * -----------
+ */
+
+// ----------------------------------------------------
+// L I S T - list posts
+//      curl http://localhost:3000/data
+// ----------------------------------------------------
+app.get("/data", function (req, res)
+{
+    // returns all data in the database
+    res.send(db.get("posts").value());
 });
 
 // ----------------------------------------------------
-// add post - test using:
-//      curl http://localhost:3000/posts/ping/1/false
+// A D D - add post - test using:
+//    curl http://localhost:3000/posts/ping/1/false
 // ----------------------------------------------------
-app.get('/posts/:title/:id/:published', function(req, res){
+app.get("/posts/:title/:id/:published", function (req, res)
+{
     var post = {
-        "id"    : req.params.id,
-        "title" : req.params.title,
-        "published" : req.params.published
-    }
+        id: Number(req.params.id),
+        title: req.params.title,
+        published: req.params.published,
+    };
     console.log(post);
-    db.get('posts').push(post).write();
-    console.log(db.get('posts').value());
-    res.send(db.get('posts').value());
+    db.get("posts").push(post).write();
+    console.log(db.get("posts").value());
+    res.send(db.get("posts").value());
 });
 
 // ----------------------------------------------------
-// filter by published state - test using:
-//      curl http://localhost:3000/published/true
+// S H O W   P U B L I S H E D - filter by published state - test using:
+//                          curl http://localhost:3000/published/true
 // ----------------------------------------------------
-app.get('/published/:boolean', function(req, res){
+app.get("/published/:boolean", function (req, res)
+{
+    var post = {
+        published: req.params.published,
+    };
 
-    // YOUR CODE
-
+    // returns all data in the database that matches 'published' state
+    res.send(db.get("posts").value());
 });
 
 // ----------------------------------------------------
-// update published value - test using:
-//      curl http://localhost:3000/published/1/true
+// S E T   P U B L I S H E D - update published value - test using:
+//                        curl http://localhost:3000/published/1/true
 // ----------------------------------------------------
-app.get('/published/:id/:boolean', function(req, res){
-
+app.get("/published/:id/:boolean", function (req, res)
+{
     // YOUR CODE
-
 });
 
 // ----------------------------------------------------
-// delete entry by id - test using:
-//      curl http://localhost:3000/delete/5
+// D E L E T E - delete entry by id - test using:
+//          curl http://localhost:3000/delete/5
+//
 // ----------------------------------------------------
-app.get('/delete/:id/', function(req, res){
-
+app.get("/delete/:id/", function (req, res)
+{
     // YOUR CODE
-
 });
 
 // start server
 // -----------------------
-app.listen(3000, function(){
-    console.log('Running at port 3000');
-})
+app.listen(3000, function ()
+{
+    console.log("Running at port 3000");
+});
--- simple.js
+++ simple.js
@@ -1,28 +1,123 @@
-var low     = require('lowdb');
-var fs      = require('lowdb/adapters/FileSync');
-var adapter = new fs('db.json');
-var db      = low(adapter);
+/*
+ * simple.js -- text module for using lowdb.
+ * ---------
+ *
+ */
+
+var low = require("lowdb");
+var fs = require("lowdb/adapters/FileSync");
+var adapter = new fs("db.json");
+var db = low(adapter);
+
+const MAX_ENTRIES = 999;
+
+// Inlcude readline to ask user questions
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
 // init the data store
-// ---------------------------
-// YOUR CODE
+// -------------------
+// Set some defaults
+db.defaults({ posts: [] }).write();
 
-// add post
-// ----------------------------
-// YOUR CODE
+// function to generate timestamp string: MM/DD/YYYY HH:MM:SS.mmm
+var timeStamp = function (x) {
+    let now = new Date();
+    return (
+        now.getMonth() +
+        1 +
+        "/" +
+        now.getDate() +
+        "/" +
+        now.getFullYear() +
+        " " +
+        now.getHours() +
+        ":" +
+        now.getMinutes() +
+        ":" +
+        now.getSeconds() +
+        "." +
+        now.getMilliseconds()
+    );
+};
+var published;
+
+// Add some posts... a lot
+for (let index = 0; index < MAX_ENTRIES; index++) {
+    published = index % 2 == 0;
+    db.get("posts")
+        .push({
+            id: index + 1,
+            title: "lowdb is awesome #" + (index + 1),
+            published: published,
+            Timestamp: timeStamp(),
+        })
+        .write();
+}
 
 // count posts
-// ----------------------------
-// YOUR CODE
+// -----------
+var entries = db.get("posts").size().value();
+
+console.log(" ");
+console.log("The DB has: " + entries.toString() + " Posts.");
+console.log(" ");
 
 // find all posts ids
-// ----------------------------
-// YOUR CODE
+// ------------------
+var id, title;
+
+for (let index = 0; index < entries; index++) {
+    id = db.get("posts[" + index + "].id").value();
+    title = db.get("posts[" + index + "].title").value();
+    console.log("Entry ID: " + id + " = TITLE: " + title);
+}
 
 // all matches of published false
-// ----------------------------
-// YOUR CODE
+// ------------------------------
+var pending = 0;
+var published = false;
+
+for (let index = 0; index < entries; index++) {
+    published = db.get("posts[" + index + "].published").value();
+    if (published == false) {
+        pending++;
+    }
+}
+
+console.log(" ");
+console.log("The DB has: " + pending + " posts needing to be published.");
 
 // find post with published false
+// ------------------------------
+
+var post = db.get("posts").find({ published: false }).value();
+
+console.log(" ");
+console.log("Entry ID: " + post.id + " = TITLE: " + post.title + ", needs to be published!");
+
+// find post with a specific ID
 // ----------------------------
-// YOUR CODE
+var searchId = MAX_ENTRIES - 4;
+
+console.log(" ");
+
+// {TBD} -- I can't get to stop for keyboard input...
+rl.question("What DB Entry do you want to see? ", function (searchId) {
+    console.log(" ");
+    console.log(`You asked for ID: ${searchId}...`);
+});
+
+var post = db.get("posts").find({ id: searchId }).value();
+
+console.log(" ");
+console.log("Entry ID: " + post.id + " = TITLE: " + post.title + ", found!");
+
+console.log(" ");
+console.log("...closing.");
+
+// Drop Console UI
+rl.close();
